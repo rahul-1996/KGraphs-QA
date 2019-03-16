48343c7a7232ba0193cde41b99e2dc235b22d733
Sahana Mohandoss <sahanamohandoss@gmail.com>
2019-03-16 21:15:57 +0530

    RelationsPreprocessing

---
+++ convert_to_pytorch_wt.py
@@ -0,0 +1,185 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import tensorflow as tf 
+import re
+import torch
+import numpy as np
+
+
+# In[2]:
+
+
+tf_path = 'weights/biobert_model.ckpt'
+
+
+# In[3]:
+
+
+init_vars = tf.train.list_variables(tf_path)
+
+
+# In[4]:
+
+
+excluded = ['BERTAdam','_power','global_step']
+init_vars = list(filter(lambda x:all([True if e not in x[0] else False for e in excluded]),init_vars))
+
+
+# In[5]:
+
+
+init_vars
+
+
+# In[25]:
+
+
+names = []
+arrays = []
+for name, shape in init_vars:
+    print("Loading TF weight {} with shape {}".format(name, shape))
+    array = tf.train.load_variable(tf_path, name)
+    names.append(name)
+    arrays.append(array)
+
+
+# In[7]:
+
+
+from pytorch_pretrained_bert  import BertConfig, BertForPreTraining
+
+
+# In[9]:
+
+
+# Initialise PyTorch model
+config = BertConfig.from_json_file('weights/bert_config.json')
+print("Building PyTorch model from configuration: {}".format(str(config)))
+model = BertForPreTraining(config)
+
+
+# In[10]:
+
+
+
+for name, array in zip(names, arrays):
+    name = name.split('/')
+    # adam_v and adam_m are variables used in AdamWeightDecayOptimizer to calculated m and v
+    # which are not required for using pretrained model
+    if any(n in ["adam_v", "adam_m", "global_step"] for n in name):
+        print("Skipping {}".format("/".join(name)))
+        continue
+    pointer = model
+    for m_name in name:
+        if re.fullmatch(r'[A-Za-z]+_\d+', m_name):
+            l = re.split(r'_(\d+)', m_name)
+        else:
+            l = [m_name]
+        if l[0] == 'kernel' or l[0] == 'gamma':
+            pointer = getattr(pointer, 'weight')
+        elif l[0] == 'output_bias' or l[0] == 'beta':
+            pointer = getattr(pointer, 'bias')
+        elif l[0] == 'output_weights':
+            pointer = getattr(pointer, 'weight')
+        else:
+            pointer = getattr(pointer, l[0])
+        if len(l) >= 2:
+            num = int(l[1])
+            pointer = pointer[num]
+    if m_name[-11:] == '_embeddings':
+        pointer = getattr(pointer, 'weight')
+    elif m_name == 'kernel':
+        array = np.transpose(array)
+    try:
+        assert pointer.shape == array.shape
+    except AssertionError as e:
+        e.args += (pointer.shape, array.shape)
+        raise
+    print("Initialize PyTorch weight {}".format(name))
+    pointer.data = torch.from_numpy(array)
+
+# Save pytorch-model
+
+
+# In[11]:
+
+
+print("Save PyTorch model to {}".format('weights/'))
+torch.save(model.state_dict(),'weights/pytorch_weight')
+
+
+# In[13]:
+
+
+import os
+import re
+import argparse
+import tensorflow as tf
+import torch
+import numpy as np
+
+from pytorch_pretrained_bert import BertConfig, BertForPreTraining
+
+def convert_tf_checkpoint_to_pytorch(tf_checkpoint_path, bert_config_file, pytorch_dump_path):
+    config_path = os.path.abspath(bert_config_file)
+    tf_path = os.path.abspath(tf_checkpoint_path)
+    print("Converting TensorFlow checkpoint from {} with config at {}".format(tf_path, config_path))
+    # Load weights from TF model
+    init_vars = tf.train.list_variables(tf_path)
+    names = []
+    arrays = []
+    for name, shape in init_vars:
+        print("Loading TF weight {} with shape {}".format(name, shape))
+        array = tf.train.load_variable(tf_path, name)
+        names.append(name)
+        arrays.append(array)
+
+    # Initialise PyTorch model
+    config = BertConfig.from_json_file(bert_config_file)
+    print("Building PyTorch model from configuration: {}".format(str(config)))
+    model = BertForPreTraining(config)
+
+    for name, array in zip(names, arrays):
+        name = name.split('/')
+        # adam_v and adam_m are variables used in AdamWeightDecayOptimizer to calculated m and v
+        # which are not required for using pretrained model
+        if any(n in ["adam_v", "adam_m", "global_step"] for n in name):
+            print("Skipping {}".format("/".join(name)))
+            continue
+        pointer = model
+        for m_name in name:
+            if re.fullmatch(r'[A-Za-z]+_\d+', m_name):
+                l = re.split(r'_(\d+)', m_name)
+            else:
+                l = [m_name]
+            if l[0] == 'kernel' or l[0] == 'gamma':
+                pointer = getattr(pointer, 'weight')
+            elif l[0] == 'output_bias' or l[0] == 'beta':
+                pointer = getattr(pointer, 'bias')
+            elif l[0] == 'output_weights':
+                pointer = getattr(pointer, 'weight')
+            else:
+                pointer = getattr(pointer, l[0])
+            if len(l) >= 2:
+                num = int(l[1])
+                pointer = pointer[num]
+        if m_name[-11:] == '_embeddings':
+            pointer = getattr(pointer, 'weight')
+        elif m_name == 'kernel':
+            array = np.transpose(array)
+        try:
+            assert pointer.shape == array.shape
+        except AssertionError as e:
+            e.args += (pointer.shape, array.shape)
+            raise
+        print("Initialize PyTorch weight {}".format(name))
+        pointer.data = torch.from_numpy(array)
+
+    # Save pytorch-model
+    print("Save PyTorch model to {}".format(pytorch_dump_path))
+    torch.save(model.state_dict(), pytorch_dump_path)
+
--- relationPreprocessing.py
+++ relationPreprocessing.py
@@ -1,14 +1,14 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 #Imports
 import sys
 
 
-# In[48]:
+# In[2]:
 
 
 #Input path declarations
@@ -17,7 +17,7 @@ relation_path = "../concept_assertion_relation_training_data/beth/rel"
 clinical_notes_path = "../concept_assertion_relation_training_data/beth/txt"
 
 
-# In[78]:
+# In[3]:
 
 
 #Read the entity tagged file -> Has actual sentences and entities
@@ -39,7 +39,7 @@ print(sents.keys())
 print(len(sents) )
 
 
-# In[180]:
+# In[4]:
 
 
 def getEntityTypes(r):   
@@ -77,12 +77,18 @@ def getEntityTypes(r):
         entity2 = "Problem"
     return entity1 , entity2
 
+def get_line_from_file(recordPath ,line):
+    lines = open(recordPath).read().strip().split('\n')
+    return lines[line-1]
+    
 
-# In[184]:
+
+# In[34]:
 
 
 #Read the relations file
 import os
+import pdb
 files = os.listdir(relation_path)
 count = 0
 for file in files:
@@ -91,6 +97,7 @@ for file in files:
     print(path)
     recordPath = clinical_notes_path + "/" + file.split(".")[0] + ".txt"
     lines = open(path).read().strip().split('\n')
+    line_numbers=[]
     for line in lines:
         print(line)
         if len(line.strip()) == 0 :
@@ -98,20 +105,37 @@ for file in files:
         entity1 = line.split("\"")[1]
         relation = line.split("\"")[3]
         entity2 = line.split("\"")[5]
-        line = int(line.split("\"")[2].lstrip().split(":")[0])
-        print(entity1, entity2, relation, line)
-        print(get_line_from_file(recordPath ,line))
-        textLine = get_line_from_file(recordPath ,line)
+        line_num = int(line.split("\"")[2].lstrip().split(":")[0])   
+        textLine = get_line_from_file(recordPath ,line_num)
         entityTypes = getEntityTypes(relation)
-        textLine=textLine.replace(entity1, entityTypes[0])
-        textLine=textLine.replace(entity2, entityTypes[1])
-        out.append(textLine + "\t" + relation)
+        textLine=textLine.lower().replace(entity1.lower(), entityTypes[0].lower())
+        textLine=textLine.lower().replace(entity2.lower(), entityTypes[1].lower())
+        line_numbers.append(line_num)     
+        out.append(textLine + "\t" + relation)    
+    lines = open(recordPath).read().strip().split('\n')
+    print(len(lines))
+    line_numbers = list(set(line_numbers))
+    print(line_numbers)
+    for i in range(len(lines)):
+        
+        print(get_line_from_file(recordPath , i) , (len(get_line_from_file(recordPath , i).strip().split())))
+        if(((i+1) in line_numbers)  or (len(get_line_from_file(recordPath , i).strip().split())<5)):
+            lines.remove(get_line_from_file(recordPath , i))
+
+        print(i)
+    print(len(lines))
+    lines= lines[:min(50, len(lines))]
+    print(len(lines))
+
+    for x in lines:
+        out.append(x + "\t" + "None")
+    
     with open("relationsTrain.tsv", 'a+') as outf:
-                outf.write(file.split(".")[0] + ":\n")
+#                 outf.write(file.split(".")[0] + ":\n")
                 for line in out:
                     outf.write(line)
                     outf.write('\n')
-                outf.write('\n\n\n\n')
+#                 outf.write('\n\n\n\n')
         
         
 
@@ -120,5 +144,5 @@ for file in files:
 # In[178]:
 
 
-
+get_line_from_file(recordPath , i)
 
